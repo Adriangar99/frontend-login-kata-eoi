b33f5c1e1a280efc22a1b775b129c1a8d8226e38
Adrián García <adriangargar99@gmail.com>
2024-04-26 19:34:09 +0200

    build: sentry config

--- react-ts/vite.config.ts
+++ react-ts/vite.config.ts
@@ -1,13 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "personal-2fl",
+    project: "javascript-react"
+  })],
+
   test: {
     environment: "jsdom",
     setupFiles: ["./tests/setup.ts"],
     exclude: ["**/node_modules/**", "**/e2e/**"],
     globals: true,
   },
+
+  build: {
+    sourcemap: true
+  }
 });
